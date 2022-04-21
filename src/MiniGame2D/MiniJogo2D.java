a3e21a080bb491ace30d6afb51a36c691b6a2056
Matheus-Ernesto <matheusernestodossantos231@gmail.com>
2022-04-21 17:55:17 -0300

    update on key listener, closed issue of keys listeners

--- src/MiniGame2D/MiniJogo2D.java
+++ src/MiniGame2D/MiniJogo2D.java
@@ -7,7 +7,7 @@ import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -20,6 +20,9 @@ public class MiniJogo2D{
 	private static JFrame janela = new JFrame();
 	private static GameEngine2D engine = new GameEngine2D();
 	public static Canvas canvas = new Canvas();
+	static GameEngine2D.KeyMapper keymanager;
+	public static KeyEvent key;
+	static int fps = 0;
 	static float vetorW = 0;
 	static float vetorA = 0;
 	static float vetorS = 0;
@@ -40,149 +43,93 @@ public class MiniJogo2D{
 			}
 		});
 		
-		
 		GameEngine2D.Scene cena = new GameEngine2D.Scene();
-		GameEngine2D.GameObject go = new GameEngine2D.GameObject();
 		janela.setVisible(true);
 		janela.add(cena);
+		janela.addKeyListener(keymanager);
 		Image tile = null;
 		try {
 			tile = ImageIO.read(MiniJogo2D.class.getResource("tile_map32.png"));
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		cena.add(go);
+		for (int iterator = 1; iterator <= 10;iterator++)  {
+			GameEngine2D.GameObject go = new GameEngine2D.GameObject();
+			go.setScale(1,iterator);
+			go.setPosition(iterator,0);
+			go.setImage(tile,go.STRETCH);
+			cena.add(go);			
+		}
+		
 		cena.setBounds(0,0,800,600);
 		cena.setBackground(Color.blue);
-		
-		go.setScale(1,1);
-		go.setPosition(0,0);
-		go.setImage(tile,go.TILED);
-		
 		cena.renderObjects(20, 0, 0, false);
-	
+		
+		engine.createRunnableStart(() -> {
+			fps++;
+		}, 99999);
+		engine.createRunnableStart(() -> {
+			System.out.println(fps + " fps");
+			fps = 0;
+		}, 1);
 		engine.createRunnableStart(() -> {
 			cena.renderObjects(cena.cameraPositionZ + vetorC + vetorV, cena.cameraPositionX + vetorA + vetorD, cena.cameraPositionY +  vetorW + vetorS, false);
-		}, 30);
+		}, 120);
 		
-		AbstractAction moverUp =  new AbstractAction() {
-			private static final long serialVersionUID = 1L;
+		keymanager = new GameEngine2D.KeyMapper() {
+			@Override
+			public void typed(KeyEvent e) {}
 
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				vetorW = 0.1f;
+			public void pressed(KeyEvent e) {
+				char key = Character.toLowerCase(e.getKeyChar());
+				switch (key) {
+				case 'w':
+					vetorW = 0.1f;
+					break;
+				case 'a':
+					vetorA = -0.1f;
+					break;
+				case 's':
+					vetorS = -0.1f;
+					break;
+				case 'd':
+					vetorD = 0.1f;
+					break;
+				default:
+					
+					break;
+				}
 			}
-		};
-		AbstractAction moverUpRel =  new AbstractAction() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				vetorW = 0f;
-			}
-		};
-		AbstractAction moverDown =  new AbstractAction() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				vetorS = -0.1f;
-			}
-		};
-		AbstractAction moverDownRel =  new AbstractAction() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				vetorS = 0f;
-			}
-		};
-		AbstractAction moverRight =  new AbstractAction() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				vetorD = 0.1f;
-			}
-		};
-		AbstractAction moverRightRel =  new AbstractAction() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				vetorD = 0f;
-			}
-		};
-		AbstractAction moverLeft =  new AbstractAction() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				vetorA = -0.1f;
-			}
-		};
-		AbstractAction moverLeftRel =  new AbstractAction() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				vetorA = 0f;
-			}
-		};
-		AbstractAction zoomUp =  new AbstractAction() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				vetorC = 0.2f;
-			}
-		};
-		AbstractAction zoomUpRel =  new AbstractAction() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				vetorC = 0f;
-			}
-		};
-		AbstractAction zoomDown =  new AbstractAction() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				vetorV = -0.2f;
-			}
-		};
-		AbstractAction zoomDownRel =  new AbstractAction() {
-			private static final long serialVersionUID = 1L;
 			
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				vetorV = 0f;
+			public void released(KeyEvent e) {
+				char key = Character.toLowerCase(e.getKeyChar());
+				switch (key) {
+				case 'w':
+					vetorW = 0f;
+					break;
+				case 'a':
+					vetorA = 0f;
+					break;
+				case 's':
+					vetorS = 0f;
+					break;
+				case 'd':
+					vetorD = 0f;
+					break;
+				default:
+					
+					break;
+				}
 			}
 		};
 		
-		GameEngine2D.KeyMapper kmp = new GameEngine2D.KeyMapper();
-		kmp.setActionByKey(cena, JComponent.WHEN_IN_FOCUSED_WINDOW, moverUp, "moveUp",KeyStroke.getKeyStroke(KeyEvent.VK_W, 0,false));
-		kmp.setActionByKey(cena, JComponent.WHEN_IN_FOCUSED_WINDOW, moverUpRel, "moveUpRel",KeyStroke.getKeyStroke(KeyEvent.VK_W, 0,true));
-		kmp.setActionByKey(cena, JComponent.WHEN_IN_FOCUSED_WINDOW, moverDown, "moveDown",KeyStroke.getKeyStroke(KeyEvent.VK_S, 0,false));
-		kmp.setActionByKey(cena, JComponent.WHEN_IN_FOCUSED_WINDOW, moverDownRel, "moveDownRel",KeyStroke.getKeyStroke(KeyEvent.VK_S, 0,true));
-		kmp.setActionByKey(cena, JComponent.WHEN_IN_FOCUSED_WINDOW, moverRight, "moveRight",KeyStroke.getKeyStroke(KeyEvent.VK_D, 0,false));
-		kmp.setActionByKey(cena, JComponent.WHEN_IN_FOCUSED_WINDOW, moverRightRel, "moveRightRel",KeyStroke.getKeyStroke(KeyEvent.VK_D, 0,true));
-		kmp.setActionByKey(cena, JComponent.WHEN_IN_FOCUSED_WINDOW, moverLeft, "moveLeft",KeyStroke.getKeyStroke(KeyEvent.VK_A, 0,false));
-		kmp.setActionByKey(cena, JComponent.WHEN_IN_FOCUSED_WINDOW, moverLeftRel, "moveLeftRel",KeyStroke.getKeyStroke(KeyEvent.VK_A, 0,true));
-		kmp.setActionByKey(cena, JComponent.WHEN_IN_FOCUSED_WINDOW, zoomUp, "zoomUp",KeyStroke.getKeyStroke(KeyEvent.VK_C, 0,false));
-		kmp.setActionByKey(cena, JComponent.WHEN_IN_FOCUSED_WINDOW, zoomUpRel, "zoomUpRel",KeyStroke.getKeyStroke(KeyEvent.VK_C, 0,true));
-		kmp.setActionByKey(cena, JComponent.WHEN_IN_FOCUSED_WINDOW, zoomDown, "zoomDown",KeyStroke.getKeyStroke(KeyEvent.VK_V, 0,false));
-		kmp.setActionByKey(cena, JComponent.WHEN_IN_FOCUSED_WINDOW, zoomDownRel, "zoomDownRel",KeyStroke.getKeyStroke(KeyEvent.VK_V, 0,true));
+		janela.addKeyListener(keymanager);
 	}
-	
 }
 
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 
 class GameEngine2D{
 	private static boolean sair = false;
@@ -205,10 +152,24 @@ class GameEngine2D{
 		sair = true;
 	}
 
-	static class KeyMapper {
-		public void setActionByKey(JComponent component,int focus, Action action, String name, KeyStroke key) {
-			component.getInputMap(focus).put(key, name);
-			component.getActionMap().put(name,action);
+	static abstract class KeyMapper extends KeyAdapter{
+		public abstract void typed(KeyEvent e);
+		public abstract void pressed(KeyEvent e);
+		public abstract void released(KeyEvent e);
+		
+		@Override
+		public void keyTyped(KeyEvent e) {
+			typed(e);
+		}
+		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			pressed(e);
+		}
+		
+		@Override
+		public void keyReleased(KeyEvent e) {
+			released(e);
 		}
 	}
 	
@@ -245,17 +206,17 @@ class GameEngine2D{
 	static class GameObject extends JComponent{
 		private static final long serialVersionUID = 1L;
 		private Image imagem;
-		public final int STRETCH = 1;
+		public final int STRETCH = 0;
 		public final int TILED = 1;
 		
 		public Rectangle r1 = new Rectangle(0,0,1,1);
 		
 		public void setImage(Image imagem_t, int style) {
 			if(style == TILED) {
-				/*Graphics g = imagem_t.getGraphics();
+				Graphics g = imagem_t.getGraphics();
 				
-				int width = this.getWidth()/ r1.width;
-				int height = this.getHeight() / r1.height;
+				int width = (int) r1.getWidth();
+				int height = (int) r1.getHeight();
 				
 				Image scaled = imagem_t.getScaledInstance(width, height, Image.SCALE_SMOOTH);
 				
@@ -264,7 +225,7 @@ class GameEngine2D{
 						g.drawImage(scaled, x, y, null);
 					}
 				}
-				g.dispose();*/
+				g.dispose();
 				this.imagem = imagem_t;				
 			}else {
 				this.imagem = imagem_t;
