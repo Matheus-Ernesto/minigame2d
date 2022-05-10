ca7b3aeef10e2de40e421e5819995e25ce080216
Matheus-Ernesto <matheusernestodossantos231@gmail.com>
2022-05-10 19:22:07 -0300

    finished

--- src/MiniGame2D/MiniJogo2D.java
+++ src/MiniGame2D/MiniJogo2D.java
@@ -1,35 +1,40 @@
 package MiniGame2D;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.Frame;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.util.Random;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.awt.Graphics;
+import java.awt.GraphicsEnvironment;
+import java.awt.Point;
 
 import javax.swing.*;
 
-public class MiniJogo2D{
+public class MiniJogo2D extends Thread{
 	private static JFrame janela = new JFrame();
 	private static GameEngine2D engine = new GameEngine2D();
 	static GameEngine2D.KeyMapper keymanager;
 	public static KeyEvent key;
 	static int fps = 0;
-	static float vetorW = 0;
-	static float vetorA = 0;
-	static float vetorS = 0;
-	static float vetorD = 0;
-	static float vetorC = 0;
-	static float vetorV = 0;
+	static Point pl;
+	static int pontos;
+	static double multPLayer = 0.01;
+	static Random rando = new Random();
+	static String move = "moveRight";
+	static String mission = "Get the blue points";
 
 	public static void main(String[] args) {
 		//Cria uma janela
 		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		janela.setTitle("Mini Jogo 2D");
 		janela.setBounds(100, 100, 800, 600);
-		janela.setLayout(new FlowLayout());
+		janela.setLayout(new BorderLayout());
 		janela.setVisible(true);
 		janela.addWindowListener(new WindowAdapter() {
 			@Override
@@ -37,120 +42,166 @@ public class MiniJogo2D{
 				engine.stop();
 			}
 		});
-
-		//Cria
+		JLabel instrucoes = new JLabel();
+		instrucoes.setText("w,a,s,d or arrows = move; r = restart; points:" + pontos + "; fps: " + fps + ";" + mission);
+		instrucoes.setFont(instrucoes.getFont().deriveFont((float) 24.0));
+		janela.add(instrucoes, BorderLayout.NORTH);
+		
+		
 		keymanager = new GameEngine2D.KeyMapper() {
 			@Override
-			public void keyTyped(KeyEvent e) {
-			}
-			
-			@Override
 			public void keyPressed(KeyEvent e) {
-				char key = Character.toLowerCase(e.getKeyChar());
+				int key = e.getKeyCode();
 				switch (key) {
-				case 'w':
-					vetorW = 0.1f;
+				case 87:
+					move = "moveUp";
 					break;
-				case 'a':
-					vetorA = -0.1f;
+				case 38:
+					move = "moveUp";
 					break;
-				case 's':
-					vetorS = -0.1f;
+				case 65:
+					move = "moveLeft";
 					break;
-				case 'd':
-					vetorD = 0.1f;
+				case 37:
+					move = "moveLeft";
 					break;
-				case 'c':
-					vetorC = -0.1f;
+				case 83:
+					move = "moveDown";
 					break;
-				case 'v':
-					vetorV = 0.1f;
+				case 40:
+					move = "moveDown";
+					break;
+				case 68:
+					move = "moveRight";
+					break;
+				case 39:
+					move = "moveRight";
+					break;
+				case 82:
+					move = "restart";
 					break;
 				default:
-					
-					break;
-				}
-			}
-			
-			@Override
-			public void keyReleased(KeyEvent e) {
-				char key = Character.toLowerCase(e.getKeyChar());
-				switch (key) {
-				case 'w':
-					vetorW = 0f;
-					break;
-				case 'a':
-					vetorA = 0f;
-					break;
-				case 's':
-					vetorS = 0f;
-					break;
-				case 'd':
-					vetorD = 0f;
-					break;
-				case 'c':
-					vetorC = 0f;
-					break;
-				case 'v':
-					vetorV = 0f;
-					break;
-				default:
-					
+					System.out.println(e.getKeyCode());
 					break;
 				}
 			}
 		};
-		
 		janela.addKeyListener(keymanager);
 		
 		GameEngine2D.Scene scene = new GameEngine2D.Scene();
 		GameEngine2D.Texture texture = new GameEngine2D.Texture();
 		GameEngine2D.RenderWindow window = new GameEngine2D.RenderWindow();
 		GameEngine2D.Camera camera = new GameEngine2D.Camera();
-		janela.add(window);
+		janela.add(window, BorderLayout.CENTER);
 		
 		texture.setBaseColor(Color.GREEN);
+		
+		GameEngine2D.GameObject border1 = new GameEngine2D.GameObject();
+		border1.setPosition(new GameEngine2D.Vector2d(0, 5));
+		border1.setScale(new GameEngine2D.Vector2d(10.5,0.5));
+		border1.setTexture(texture);
+		border1.setName("Border");
+		scene.addObject(border1);
+		
+		GameEngine2D.GameObject border2 = new GameEngine2D.GameObject();
+		border2.setPosition(new GameEngine2D.Vector2d(0,-5));
+		border2.setScale(new GameEngine2D.Vector2d(10.5,0.5));
+		border2.setTexture(texture);
+		border2.setName("Border");
+		scene.addObject(border2);
+		
+		GameEngine2D.GameObject border3 = new GameEngine2D.GameObject();
+		border3.setPosition(new GameEngine2D.Vector2d(5, 0));
+		border3.setScale(new GameEngine2D.Vector2d(0.5,10.5));
+		border3.setTexture(texture);
+		border3.setName("Border");
+		scene.addObject(border3);
+		
+		GameEngine2D.GameObject border4 = new GameEngine2D.GameObject();
+		border4.setPosition(new GameEngine2D.Vector2d(- 5, 0));
+		border4.setScale(new GameEngine2D.Vector2d(0.5,10.5));
+		border4.setTexture(texture);
+		border4.setName("Border");
+		scene.addObject(border4);
 
-		for (int cont = 0;cont <= 3; cont++) {
-			GameEngine2D.GameObject gmObject = new GameEngine2D.GameObject();
-			gmObject.setPosition(new GameEngine2D.Vector2d(cont,0));
-			gmObject.setScale(new GameEngine2D.Vector2d(1,1));
-			gmObject.setTexture(texture);
-			scene.addObject(gmObject);
-		}
+		GameEngine2D.Texture texturePlayer = new GameEngine2D.Texture();
+		texturePlayer.setBaseColor(Color.RED);
+		
+		GameEngine2D.GameObject player = new GameEngine2D.GameObject();
+		player.setPosition(new GameEngine2D.Vector2d(0,0));
+		player.setScale(new GameEngine2D.Vector2d(0.5,0.5));
+		player.setTexture(texturePlayer);
+		player.setName("Player");
+		scene.addObject(player);
+		
+		GameEngine2D.Texture texturePoint = new GameEngine2D.Texture();
+		texturePoint.setBaseColor(Color.BLUE);
+		
+		GameEngine2D.GameObject point = new GameEngine2D.GameObject();
+		point.setPosition(new GameEngine2D.Vector2d(rando.nextInt(10) - 4.5,rando.nextInt(10) - 4.5));
+		point.setScale(new GameEngine2D.Vector2d(0.5,0.5));
+		point.setTexture(texturePoint);
+		point.setName("point");
+		scene.addObject(point);
 		
 		camera.setPosition(new GameEngine2D.Vector2d(0,0));
-		camera.setRadius(12);
-		camera.setStretch(false);
+		camera.setRadius(15);
 		
-		window.setBounds(0,0,800,600);
 		window.setBackground(Color.GRAY);
-
 
 		window.setScene(scene);
 		window.setCamera(camera);
 		window.RenderFrame(99999);
 		
 		engine.createRunnableStart(() -> {
-			System.out.println(window.getFramerate() + " fps");
-			fps = 0;
+			instrucoes.setText("w,a,s,d or arrows = move; r = restart; points:" + pontos + "; fps: " + window.getFramerate() + ";" + mission);
 		}, 1);
 		
 		engine.createRunnableStart(() -> {
-			double x = camera.getPosition().getX() + vetorA + vetorD;
-			double y = camera.getPosition().getY() + vetorS + vetorW;
-			double z = camera.getRadius() + vetorC + vetorV;
-			camera.setPosition(new GameEngine2D.Vector2d(x, y));
-			camera.setRadius(z);
+			//Border Game Over
+			if(player.getPosition().getX() >= 4.5 || player.getPosition().getY() >= 4.5 || player.getPosition().getX() <= -4.5 || player.getPosition().getY() <= -4.5) {
+				multPLayer=0;
+				mission = "You died!";
+			}
+			
+			//Move Player
+			switch (move) {
+			case "moveUp":
+				player.setPosition(new GameEngine2D.Vector2d(player.getPosition().getX(), player.getPosition().getY() - multPLayer));
+				break;
+			case "moveRight":
+				player.setPosition(new GameEngine2D.Vector2d(player.getPosition().getX() + multPLayer, player.getPosition().getY()));
+				break;
+			case "moveDown":
+				player.setPosition(new GameEngine2D.Vector2d(player.getPosition().getX(), player.getPosition().getY() + multPLayer));
+				break;
+			case "restart":
+				point.setPosition(new GameEngine2D.Vector2d(rando.nextInt(10) - 4.5,rando.nextInt(10) - 4.5));
+				pontos = 0;
+				multPLayer = 0.01;
+				move = "moveRight";
+				mission = "Get the blue points";
+				player.setPosition(new GameEngine2D.Vector2d(0,0));
+				break;
+			default:
+				player.setPosition(new GameEngine2D.Vector2d(player.getPosition().getX() - multPLayer, player.getPosition().getY() ));
+				break;
+			}
+			
+			//Point
+			if(player.getPosition().getY() >= point.getPosition().getY()-0.5 && player.getPosition().getY() <= point.getPosition().getY()+0.5 
+					&& player.getPosition().getX() >= point.getPosition().getX()-0.5 && player.getPosition().getX() <= point.getPosition().getX()+0.5) {
+				multPLayer+=0.01;
+				point.setPosition(new GameEngine2D.Vector2d(rando.nextInt(10) - 4.5,rando.nextInt(10) - 4.5));
+				pontos++;
+			}
 		}, 120);
-		
+		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		janela.setMaximizedBounds(env.getMaximumWindowBounds());
+		janela.setExtendedState(janela.getExtendedState() | Frame.MAXIMIZED_BOTH);
+         
 	}
 }
-
-
-
-
-
 
 class GameEngine2D{
 	private static boolean sair = false;
@@ -174,6 +225,8 @@ class GameEngine2D{
 	}
 
 	static abstract class KeyMapper extends KeyAdapter{}
+	
+	static abstract class MouseMapper extends MouseAdapter{}
 	
 	static class Scene{
 		ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
@@ -215,6 +268,15 @@ class GameEngine2D{
 		private Vector2d position;
 		private Vector2d scale;
 		private Vector2d rotation;
+		private String name;
+		
+		public void setName(String name_t) {
+			this.name = name_t;
+		}
+		
+		public String getName(){
+			return this.name;
+		}
 		
 		public void setPosition(Vector2d pos_t) {
 			this.position = pos_t;
@@ -243,7 +305,6 @@ class GameEngine2D{
 	
 	static class Camera extends Object{
 		private double radius = 0;
-		private boolean stretch = false;
 		
 		public void setRadius(double rad_t) {
 			this.radius = rad_t;
@@ -251,14 +312,6 @@ class GameEngine2D{
 		
 		public double getRadius(){
 			return this.radius;
-		}
-		
-		public void setStretch(boolean stretch_t){
-			this.stretch = stretch_t;
-		}
-		
-		public boolean getStretch(){
-			return this.stretch;
 		}
 	}
 	
@@ -275,10 +328,6 @@ class GameEngine2D{
 		
 	}
 	
-	static class Light extends Object{
-		
-	}
-	
 	static class Texture{
 		private Color baseColor;
 		
@@ -291,7 +340,7 @@ class GameEngine2D{
 		}
 	}
 	
-	static class RenderWindow extends JPanel{
+	static class RenderWindow extends JPanel {
 		
 		private static final long serialVersionUID = 1L;
 		
@@ -310,6 +359,15 @@ class GameEngine2D{
 			this.camera = camera_t;
 		}
 		
+		public Vector2d pixelToMeter(Point p) {
+			double z = camera.getRadius();
+			
+			double MX = ((p.getX() / this.getWidth()) * 100 * z) / 100 - (z/2);
+			double MY = ((p.getY() / this.getHeight()) * 100 * z) / 100 - (z/2);
+			
+			return new Vector2d(MX,MY);
+		}
+		
 		@Override
 		protected void paintComponent(Graphics g) {
 			this.fps++;
@@ -317,19 +375,20 @@ class GameEngine2D{
 			double x = camera.getPosition().getX();
 			double y = camera.getPosition().getY();
 			double z = camera.getRadius();
-			
-			double PartX = this.getWidth() / z;
-			double PartY = (camera.getStretch()) ? this.getHeight() / z: this.getWidth() / z;
-			
-			double X_cart = x + z / 2;
+
+			double PartX = this.getHeight() / z;
+			double PartY = this.getHeight() / z;
+			double X_cart = x + z;
 			double Y_cart = -y + z / 2;
+			
 			ArrayList<GameObject> comps = scene.getObjectsOnRadius(new Vector2d(x,y), z);
 			for (GameObject comp : comps) {
-				int width = (int) (PartX * comp.getScale().getX());
-				int height = (int) (PartY * comp.getScale().getY());
+				int width = (int) Math.round(PartX * comp.getScale().getX());
+				int height = (int) Math.round(PartY * comp.getScale().getY());
 				
-				int x_local = (int) (PartX * (comp.getPosition().getX() + X_cart) - width/2);
-				int y_local = (int) (PartY * (comp.getPosition().getY() + Y_cart) - height/2);
+				int x_local = (int) Math.round(PartX * (comp.getPosition().getX() + X_cart) - width/2);
+				int y_local = (int) Math.round(PartY * (comp.getPosition().getY() + Y_cart) - height/2);
+				
 				g.setColor(comp.getTexture().getBaseColor());
 				g.fillRect(x_local, y_local, width, height);
 			}
@@ -358,10 +417,9 @@ class GameEngine2D{
 			fps = 0;
 			return fps_t;
 		}
-		
 
 	}
-	
+
 	static class Vector2d {
 		private double x = 0;
 		private double y = 0;
@@ -390,6 +448,13 @@ class GameEngine2D{
 		
 		public double getY() {
 			return this.y;
+		}
+		
+		public double hipotenusa(Vector2d catect) {
+			double side_a = Math.abs(this.getX() - catect.getX());
+			double side_b = Math.abs(this.getY() - catect.getY());
+			double side_hipo = (side_a * side_a) + (side_b * side_b);
+			return side_hipo;
 		}
 	}
 }
